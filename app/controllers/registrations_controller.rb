1472a644aeec355d7cb6ffd3505fb6916a719904
o.volosianskyi <o.volosianskyi@sloboda-studio.com>
2020-03-20 12:36:15 +0200

    configured sign up modal

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,3 +1,13 @@
 class RegistrationsController < Devise::RegistrationsController
   respond_to :html, :json
+
+  def new
+    render '../views/shared/_sign_up'
+  end
+
+  def create
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password])
+    super
+  end
+  
 end
